d50fc330d17d6af96ef06c56d3c95a1befde3b2e
Andres Oe <acojeda.6773@unicesmag.edu.com>
2024-04-19 10:58:32 -0500

    database and singup

--- src/backend/singup.php
+++ src/backend/singup.php
@@ -1,11 +1,25 @@
 <?php
-    $fullname=$_POST['fnam'];
+    include('../../config/database.php');
+
+    $fullname=$_POST['fname'];
     $email=$_POST['email'];
     $passwd=$_POST['passwd'];
     $enc_pass = md5($passwd);
 
-    echo "your fullname:".$fullname. "<br>";
-    echo "your email:". $email. "<br>";
-    echo "your password enc:". $enc_pass;
+    $sql = "
+    INSERT INTO users (fullname, email, password) 
+        VALUES ('$fullname', '$email','$enc_pass')
+    ";
+
+    $ans = pg_query($conn,$sql);
+    if ($ans){
+        echo "User has been created successfully";
+    }else{
+        echo "Error: " . pg_last_error();
+}
+
+//Close connection
+pg_close($conn)
+
 
 ?>
