a2bd428f987d9643774789fb877d834c3e22587a
Andres Oe <acojeda.6773@unicesmag.edu.com>
2024-05-03 11:47:14 -0500

    Actualizando proyecto

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -6,17 +6,27 @@
     $passwd=$_POST['passwd'];
     $enc_pass = md5($passwd);
 
-    $sql = "
-    INSERT INTO users (fullname, email, password) 
-        VALUES ('$fullname', '$email','$enc_pass')
-    ";
+    $sql_value_email = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $sql_value_email);
+    $total = pg_num_rows($result);
 
-    $ans = pg_query($conn,$sql);
-    if ($ans){
-        echo "<script>alert('user has been registerd')</script>";
-        header("refresh: 0; url= ../signin.html");
+    if ($total > 0){
+        echo "<script>alert('Email already exists')</script>";
+        header("refresh:0;url=../signup.html");
     }else{
-        echo "Error: " . pg_last_error();
+        $sql = "
+        INSERT INTO users (fullname, email, password) 
+            VALUES ('$fullname', '$email','$enc_pass')
+        ";
+    
+        $ans = pg_query($conn,$sql);
+        if ($ans){
+            echo "<script>alert('user has been registerd')</script>";
+            header("refresh:0;url=../signin.html");
+        }else{
+            echo "Error: " . pg_last_error();
+
+    }
 }
 
 //Close connection
